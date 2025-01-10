d3a4bdffeebf2f59bd5d7d9ec7588ffbebdba523
Yong-Zhuang <jazz14jazz@gmail.com>
2025-01-10 02:05:03 +0000

    Deploying to gh-pages from @ iadsl/capstones@1de87826848620941e3956226eeda677ae3995a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,14 +7,14 @@ ninja.data = [{
     title: "Home",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/capstone/";
+      window.location.href = "/capstones/";
     },
   },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of the projects undertaken by undergraduate and graduate students under my supervision.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/capstone/projects/";
+            window.location.href = "/capstones/projects/";
           },
         },{id: "post-a-post-with-image-galleries",
       
@@ -24,7 +24,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/photo-gallery/";
+          window.location.href = "/capstones/blog/2024/photo-gallery/";
         
       },
     },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
@@ -46,7 +46,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/tabs/";
+          window.location.href = "/capstones/blog/2024/tabs/";
         
       },
     },{id: "post-a-post-with-typograms",
@@ -57,7 +57,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/typograms/";
+          window.location.href = "/capstones/blog/2024/typograms/";
         
       },
     },{id: "post-a-post-that-can-be-cited",
@@ -68,7 +68,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/post-citation/";
+          window.location.href = "/capstones/blog/2024/post-citation/";
         
       },
     },{id: "post-a-post-with-pseudo-code",
@@ -79,7 +79,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/pseudocode/";
+          window.location.href = "/capstones/blog/2024/pseudocode/";
         
       },
     },{id: "post-a-post-with-code-diff",
@@ -90,7 +90,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/code-diff/";
+          window.location.href = "/capstones/blog/2024/code-diff/";
         
       },
     },{id: "post-a-post-with-advanced-image-components",
@@ -101,7 +101,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/advanced-images/";
+          window.location.href = "/capstones/blog/2024/advanced-images/";
         
       },
     },{id: "post-a-post-with-vega-lite",
@@ -112,7 +112,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/vega-lite/";
+          window.location.href = "/capstones/blog/2024/vega-lite/";
         
       },
     },{id: "post-a-post-with-geojson",
@@ -123,7 +123,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/geojson-map/";
+          window.location.href = "/capstones/blog/2024/geojson-map/";
         
       },
     },{id: "post-a-post-with-echarts",
@@ -134,7 +134,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/echarts/";
+          window.location.href = "/capstones/blog/2024/echarts/";
         
       },
     },{id: "post-a-post-with-chart-js",
@@ -145,7 +145,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2024/chartjs/";
+          window.location.href = "/capstones/blog/2024/chartjs/";
         
       },
     },{id: "post-a-post-with-tikzjax",
@@ -156,7 +156,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/tikzjax/";
+          window.location.href = "/capstones/blog/2023/tikzjax/";
         
       },
     },{id: "post-a-post-with-bibliography",
@@ -167,7 +167,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/post-bibliography/";
+          window.location.href = "/capstones/blog/2023/post-bibliography/";
         
       },
     },{id: "post-a-post-with-jupyter-notebook",
@@ -178,7 +178,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/jupyter-notebook/";
+          window.location.href = "/capstones/blog/2023/jupyter-notebook/";
         
       },
     },{id: "post-a-post-with-custom-blockquotes",
@@ -189,7 +189,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/custom-blockquotes/";
+          window.location.href = "/capstones/blog/2023/custom-blockquotes/";
         
       },
     },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
@@ -200,7 +200,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/sidebar-table-of-contents/";
+          window.location.href = "/capstones/blog/2023/sidebar-table-of-contents/";
         
       },
     },{id: "post-a-post-with-audios",
@@ -211,7 +211,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/audios/";
+          window.location.href = "/capstones/blog/2023/audios/";
         
       },
     },{id: "post-a-post-with-videos",
@@ -222,7 +222,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/videos/";
+          window.location.href = "/capstones/blog/2023/videos/";
         
       },
     },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
@@ -233,7 +233,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/tables/";
+          window.location.href = "/capstones/blog/2023/tables/";
         
       },
     },{id: "post-a-post-with-table-of-contents",
@@ -244,7 +244,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2023/table-of-contents/";
+          window.location.href = "/capstones/blog/2023/table-of-contents/";
         
       },
     },{id: "post-a-post-with-giscus-comments",
@@ -255,7 +255,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2022/giscus-comments/";
+          window.location.href = "/capstones/blog/2022/giscus-comments/";
         
       },
     },{id: "post-displaying-external-posts-on-your-al-folio-blog",
@@ -277,7 +277,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/assets/pdf/example_pdf.pdf";
+          window.location.href = "/capstones/assets/pdf/example_pdf.pdf";
         
       },
     },{id: "post-a-post-with-diagrams",
@@ -288,7 +288,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2021/diagrams/";
+          window.location.href = "/capstones/blog/2021/diagrams/";
         
       },
     },{id: "post-a-distill-style-blog-post",
@@ -299,7 +299,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2021/distill/";
+          window.location.href = "/capstones/blog/2021/distill/";
         
       },
     },{id: "post-a-post-with-twitter",
@@ -310,7 +310,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2020/twitter/";
+          window.location.href = "/capstones/blog/2020/twitter/";
         
       },
     },{id: "post-a-post-with-disqus-comments",
@@ -321,7 +321,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2015/disqus-comments/";
+          window.location.href = "/capstones/blog/2015/disqus-comments/";
         
       },
     },{id: "post-a-post-with-math",
@@ -332,7 +332,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2015/math/";
+          window.location.href = "/capstones/blog/2015/math/";
         
       },
     },{id: "post-a-post-with-code",
@@ -343,7 +343,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2015/code/";
+          window.location.href = "/capstones/blog/2015/code/";
         
       },
     },{id: "post-a-post-with-images",
@@ -354,7 +354,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2015/images/";
+          window.location.href = "/capstones/blog/2015/images/";
         
       },
     },{id: "post-a-post-with-formatting-and-links",
@@ -365,7 +365,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/capstone/blog/2015/formatting-and-links/";
+          window.location.href = "/capstones/blog/2015/formatting-and-links/";
         
       },
     },{id: "news-a-simple-inline-announcement",
@@ -375,7 +375,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/capstone/news/announcement_2/";
+              window.location.href = "/capstones/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -383,92 +383,92 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/1_project/";
+              window.location.href = "/capstones/projects/1_project/";
             },},{id: "projects-expensetracker",
           title: 'ExpenseTracker',
           description: "A comprehensive web application for tracking and managing personal finances.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2024f-1/";
+              window.location.href = "/capstones/projects/2024f-1/";
             },},{id: "projects-gvtextsummary",
           title: 'GVTextSummary',
           description: "AI-Powered Multilingual Text Summarization and Translation Tool",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2024f-2/";
+              window.location.href = "/capstones/projects/2024f-2/";
             },},{id: "projects-crimepredict",
           title: 'CrimePredict+',
           description: "A study comparing 3D CNN, RNN, and CNN+RNN models for crime hotspot prediction.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2024w-1/";
+              window.location.href = "/capstones/projects/2024w-1/";
             },},{id: "projects-plan-a-trip",
           title: 'Plan A Trip',
           description: "A user-friendly platform for travelers to collaborate, plan, and share their trip experiences.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2024w-2/";
+              window.location.href = "/capstones/projects/2024w-2/";
             },},{id: "projects-kshira",
           title: 'KSHIRA',
           description: "Revolutionizing the dairy industry with a digital platform for sustainable practices and enhanced animal welfare.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2024w-3/";
+              window.location.href = "/capstones/projects/2024w-3/";
             },},{id: "projects-melodymaker",
           title: 'MelodyMaker',
           description: "Interactive music generation with AI using MusicVAE.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2025w-1/";
+              window.location.href = "/capstones/projects/2025w-1/";
             },},{id: "projects-crimepredict",
           title: 'CrimePredict',
           description: "Predicting future crime hotspots using advanced machine learning models.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2025w-2/";
+              window.location.href = "/capstones/projects/2025w-2/";
             },},{id: "projects-rainfallpredict",
           title: 'RainfallPredict',
           description: "Predicting rainfall patterns using Indonesia&#39;s climate data for sustainable solutions.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2025w-3/";
+              window.location.href = "/capstones/projects/2025w-3/";
             },},{id: "projects-giftcardplus",
           title: 'GiftCardPlus',
           description: "A web application for purchasing and utilizing gift cards with integrated discounts.",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2025w-4/";
+              window.location.href = "/capstones/projects/2025w-4/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/2_project/";
+              window.location.href = "/capstones/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/3_project/";
+              window.location.href = "/capstones/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/4_project/";
+              window.location.href = "/capstones/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/5_project/";
+              window.location.href = "/capstones/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/6_project/";
+              window.location.href = "/capstones/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/7_project/";
+              window.location.href = "/capstones/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/8_project/";
+              window.location.href = "/capstones/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/capstone/projects/9_project/";
+              window.location.href = "/capstones/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -488,7 +488,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/capstone/feed.xml", "_blank");
+          window.open("/capstones/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
